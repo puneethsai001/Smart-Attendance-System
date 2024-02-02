4f76916877c1bf5e78590c8de7b5343dfe8d8255
abhay <abhaykamath47@gmail.com>
2024-02-02 13:08:23 +0400

    Complete Main2.py

--- Main2.py
+++ Main2.py
@@ -2,12 +2,13 @@ import face_recognition
 import pandas as pd
 
 df = pd.read_csv('Student.csv', delimiter=',')
-
+presentees=[]
+absentees=[]
 for index, row in df.iterrows():
     StudPath = row['File Path']
 
     knownImage = face_recognition.load_image_file(StudPath)
-    unknownImage = face_recognition.load_image_file("Reference Images\Daniel_Emma_Rupert.jpg")
+    unknownImage = face_recognition.load_image_file("Reference Images\Scarlet_Chris.jpg")
 
     knownEncoding = face_recognition.face_encodings(knownImage)[0]
     unknownEncoding = face_recognition.face_encodings(unknownImage)
@@ -15,5 +16,22 @@ for index, row in df.iterrows():
     for i in range(0,len(unknownEncoding)):
         results = face_recognition.compare_faces([knownEncoding], unknownEncoding[i])
         if results[0] == True:
-            print(row['Name'])
+            presentees.append([row["Reg No"],row["Name"]])
             break
+    list=[row["Reg No"],row["Name"]]
+    if list not in presentees:
+        absentees.append(list)
+
+pr_df=pd.DataFrame(columns=["Reg No","Name"])
+abs_df=pd.DataFrame(columns=["Reg No","Name"])
+
+for i in presentees:
+    pr_df.loc[len(pr_df)] = i
+
+for j in absentees:
+    abs_df.loc[len(abs_df)] = j
+
+pr_df.to_csv("Presentees.csv", sep=',', index=False, encoding='utf-8')
+abs_df.to_csv("Absentees.csv", sep=',', index=False, encoding='utf-8')
+
+print("Success")
