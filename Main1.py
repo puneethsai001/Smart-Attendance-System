0fa8215fae369c20a54d539ff20f57d263c5fccd
abhay <abhaykamath47@gmail.com>
2024-02-02 13:21:10 +0400

    Completed Main1.py

--- Main1.py
+++ Main1.py
@@ -3,8 +3,31 @@ from deepface import DeepFace
 
 df = pd.read_csv('Student.csv', delimiter=',')
 
+presentees=[]
+absentees=[]
+
 for index, row in df.iterrows():
     StudPath = row['File Path']
     result = DeepFace.verify(img1_path = StudPath, img2_path = "Reference Images\Richard_Kit.jpg")
     if result.get('verified') == True:
-        print(row['Name'])
+        presentees.append([row["Reg No"],row["Name"]])
+    else:
+        absentees.append([row["Reg No"],row["Name"]])
+
+pr_df = pd.DataFrame(columns=["Reg No","Name"])
+abs_df = pd.DataFrame(columns=["Reg No","Name"])
+
+for i in presentees:
+    pr_df.loc[len(pr_df)] = i
+
+for j in absentees:
+    abs_df.loc[len(abs_df)] = j
+
+pr_df.to_csv("Presentees.csv", sep=',', index=False, encoding='utf-8')
+abs_df.to_csv("Absentees.csv", sep=',', index=False, encoding='utf-8')
+
+print("Success")
+
+        
+
+        
